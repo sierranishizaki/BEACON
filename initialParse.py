b3298030e10a415d5ee725b02be878d925976759
Pelle Hall <hallpell@gmail.com>
2025-09-05 21:50:15 -0700

    Clean up UI

--- initialParse.py
+++ initialParse.py
@@ -1,8 +1,36 @@
 import gzip
+import sys
+import getopt
 import os
 import time
 from pathlib import Path
 
+filein = ''
+threshold = 900
+cache_dir = 'cache'
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hi:t:c:", ["help", "input=", "threshold=", "cache="])
+except getopt.GetoptError:
+    print('python initialParse.py -i INPUTFILE [-t INTEGER0-1000] [-c CACHEDIR]')
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        print("python initialParse.py -i INPUTFILE [-t INTEGER0-1000] [-c CACHEDIR]\n" +
+              "INPUTFILE can be .txt or .txt.gz, threshold defaults to 900, cache defaults to 'cache'")
+        sys.exit()
+    elif opt in ("-i", "--input"):
+        filein = arg
+    elif opt in ("-t", "--threshold"):
+        threshold = int(arg)
+    elif opt in ("-c", "--cache"):
+        cache_dir = arg
+
+if not os.path.exists(filein):
+    print("Input file '" + filein + "' was not found")
+    sys.exit()
+        
 tic = time.time()
 
 allProt = set()
@@ -10,11 +38,20 @@ allProt = set()
 counter = 0
 modVal = 1000000
 
-if len(os.listdir("cache/")) != 0:
-    print("cache was not empty - are you sure you didn't want to delete it first?")
+if not os.path.exists(cache_dir):
+    os.mkdir(cache_dir)
+if len(os.listdir(cache_dir)) != 0:
+    print(cache, "was not an empty directory, please delete files in", cache_dir, "or use a different directory as a cache")
 
-with gzip.open("9606.protein.links.v12.0.txt.gz", 'rt') as f:
-    for line in f:
+if filein[-4:] == '.txt':
+    f = open(filein, 'r')
+elif filein[-7:] == '.txt.gz':
+    f = gzip.open(filein, 'rt')
+else:
+    print("Unrecognized file format for '" + filein + "', please use .txt or .txt.gz")
+    sys.exit()
+    
+for line in f:
         counter += 1
         if counter == 1:
             continue
@@ -24,14 +61,14 @@ with gzip.open("9606.protein.links.v12.0.txt.gz", 'rt') as f:
         allProt.add(prot1)
         allProt.add(prot2)
         if prot1 < prot2 and int(score) >= 900:
-            with open("cache/"+prot1, "a") as w:
+            with open(cache_dir+"/"+prot1, "a") as w:
                 w.write(prot2 + "\t1\n")
-            with open("cache/"+prot2, "a") as w:
+            with open(cache_dir+"/"+prot2, "a") as w:
                 w.write(prot1 + "\t1\n")
         
-with open("allProteins.txt", "w") as f:
+with open(cache_dir + "/allProteins.txt", "w") as f:
     for p in sorted(allProt):
         print(p, file=f)
-        Path("cache/"+p).touch()
+        Path(cache_dir+"/"+p).touch()
 
 print(time.time()-tic)
--- pathfind.py
+++ pathfind.py
@@ -1,7 +1,35 @@
 import os
+import sys
 import time
+import getopt
+
+inputCache = 'cache'
+outputCache = 'output'
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["help", "input=", "output="])
+except getopt.GetoptError:
+    print('python pathfind.py [-i INPUTCACHEDIR] [-o OUTPUTDIR]')
+    sys.exit(2)
+    
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        print("python pathfind.py [-i INPUTCACHE] [-o OUTPUTDIR]\n" +
+              "INPUTCACHE defaults to 'cache', OUTPUT defaults to 'output'")
+        sys.exit()
+    elif opt in ("-i", "--input"):
+        inputCache = arg
+    elif opt in ("-o", "--output"):
+        outputCache = arg
+
+if not os.path.exists(outputCache):
+    os.mkdir(outputCache)
+if len(os.listdir(outputCache)) != 0:
+    print(outputCache, "was not an empty directory, please delete files in", outputCache, "or use a different directory as a cache")
 
 allProt = []
+
+# this PQ a bit too fancy for the simple pathfinding algorithm that
+#   ended up being used for this, it could have just been a flat queue
 
 # pq is a priority queue organized to be a list of lists
 #   the priority is an integer (number of steps from source)
@@ -23,61 +51,55 @@ def popPQ(pq):
         if len(pq[i]) > 0:
             return (pq[i].pop(), i)
 
-if not os.path.exists("cache2"):
-    os.mkdir("cache2")
-if len(os.listdir("cache2/")) != 0:
-    print("cache2 is not empty!!!!")
-        
 tic = time.time()
 counter = 0
 
 bigCache = {}
-with open("allProteins.txt", 'r') as allPFile:
+with open(inputCache + "/allProteins.txt", 'r') as allPFile:
     for p in allPFile:
-        p = p[:-1]
-        with open("cache/" + p, 'r') as f:
+        if p[-1] == "\n":
+            p = p[:-1]
+        with open(inputCache + "/" + p, 'r') as f:
             bigCache[p] = []
             for d in f:
                 bigCache[p].append(d.split()[0])
 
-with open("allProteins.txt", 'r') as allPFile:
-    for source in allPFile:
-        counter += 1
-        if counter % 100 == 0:
-            print(counter, "\t", time.time()-tic)
-        source = source[:-1] # trim off newline
+for source in sorted(bigCache.keys()):
+    counter += 1
+    if counter % 100 == 0:
+        print(counter, "\t", time.time()-tic)
 
-        distances = {source: 0}
-        pq = [[source]]
-        innerCounter = 0
-        while sizePQ(pq) > 0:
-            innerCounter += 1
-            #if innerCounter % 1000 == 0:
-            #    print(source, innerCounter, sizePQ(pq), time.time()-tic, sep="\t")
-            (curProt, curSteps) = popPQ(pq)
-            # if this protein doesn't have any interactions, skip it
-            #   (this should only happen for proteins at step 0)
-            #if not os.path.exists("cache/" + curProt):
-            #    continue
-            # if we've already seen this protein, skip it
-            #   (the distance part shouldn't be necessary due to priority queue,
-            #    but it is cheap to check just in case) TODO: remove?
-            #if curProt != source and curProt in distances and curSteps >= distances[curProt]:
-            #    print("Prevented revisiting", curProt)
-            #    continue
+    distances = {source: 0}
+    pq = [[source]]
+    innerCounter = 0
+    while sizePQ(pq) > 0:
+        innerCounter += 1
+        #if innerCounter % 1000 == 0:
+        #    print(source, innerCounter, sizePQ(pq), time.time()-tic, sep="\t")
+        (curProt, curSteps) = popPQ(pq)
+        # if this protein doesn't have any interactions, skip it
+        #   (this should only happen for proteins at step 0)
+        #if not os.path.exists("cache/" + curProt):
+        #    continue
+        # if we've already seen this protein, skip it
+        #   (the distance part shouldn't be necessary due to priority queue,
+        #    but it is cheap to check just in case) TODO: remove?
+        #if curProt != source and curProt in distances and curSteps >= distances[curProt]:
+        #    print("Prevented revisiting", curProt)
+        #    continue
 
-            for nextProt in bigCache[curProt]:
-                nextStep = 1
-                # if we already know a faster way to get to this protein, skip it
-                #   before the and prevents backtracking
-                #   TODO: verify if the distance stuff adds value
-                if nextProt in distances:
-                    continue
-                addPQ(pq, nextProt, curSteps + 1)
-                distances[nextProt] = curSteps + 1
-            f.close()
-        with open("cache2/" + source, "w") as f:
-            # sort the dictionary by value, then print
-            sorted_items = sorted(distances.items(), key=lambda item: item[1])
-            for prot, steps in sorted_items:
-                print(prot, steps, file=f)
+        for nextProt in bigCache[curProt]:
+            nextStep = 1
+            # if we already know a faster way to get to this protein, skip it
+            #   before the and prevents backtracking
+            #   TODO: verify if the distance stuff adds value
+            if nextProt in distances:
+                continue
+            addPQ(pq, nextProt, curSteps + 1)
+            distances[nextProt] = curSteps + 1
+        f.close()
+    with open(outputCache + "/" + source, "w") as f:
+        # sort the dictionary by value, then print
+        sorted_items = sorted(distances.items(), key=lambda item: item[1])
+        for prot, steps in sorted_items:
+            print(prot, steps, file=f)
